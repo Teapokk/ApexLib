0c90dbf128e126f8b0a15cc2131ec18a1346cf4d
gaab <gn375294@gmail.com>
2026-08-20 20:16:48 -0300

    Update version number to 1.2.1

--- ApexLib.lua
+++ ApexLib.lua
@@ -1,6 +1,6 @@
 local ApexLib = {}
 ApexLib.__index = ApexLib
-ApexLib.Version = "1.1.1"
+ApexLib.Version = "1.2.1"
 ApexLib.Author = "Teapokk"
 ApexLib.BuildDate = os.date("%Y-%m-%d")
 ApexLib.Github = "https://github.com/Teapokk/ApexLib"
